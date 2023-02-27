35ec78e0ca3bfb782bd57b7cf36d545ae658a486
Fairuz <akbarazariafairuz@gmail.com>
2023-02-27 22:30:34 +0700

    20230222

--- app/src/main/java/com/example/fragmentexample1updated/SimpleFragment.java
+++ app/src/main/java/com/example/fragmentexample1updated/SimpleFragment.java
@@ -19,6 +19,10 @@ public class SimpleFragment extends Fragment {
         // Required empty public constructor
     }
 
+    public static SimpleFragment newInstance() {
+        return new SimpleFragment();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
