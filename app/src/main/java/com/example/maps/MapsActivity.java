d67f234d3884075fa6229674d7a650a4c9fabb53
pipitgoku <pipitbaik@gmail.com>
2023-04-08 12:02:24 +0700

    20230329

--- app/src/main/java/com/example/maps/MapsActivity.java
+++ app/src/main/java/com/example/maps/MapsActivity.java
@@ -7,8 +7,14 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -17,7 +23,10 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.maps.databinding.ActivityMapsBinding;
@@ -56,13 +65,41 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
         LatLng vokasi = new LatLng(-7.774621410988136, 110.37448061108397);
-        mMap.addMarker(new MarkerOptions().position(vokasi).title("Marker in Vokasi"));
+        LatLng ugm = new LatLng(-7.770789235420296, 110.37763635355337);
+        mMap.addMarker(new MarkerOptions()
+                .position(vokasi)
+                .title("Marker in Vokasi")
+                .icon(BitmapFromVector(getApplicationContext(), R.drawable.baseline_school))
+        );
+        mMap.addMarker(new MarkerOptions().position(ugm).title("Marker in UGM"));
+
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(vokasi,20));
         setMapLongClick(mMap);
         setPoiClick(mMap);
         enableMyLocation();
+    }
+
+    private BitmapDescriptor BitmapFromVector(Context context, int vectorResId) {
+        // below line is use to generate a drawable.
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+
+        // below line is use to set bounds to our vector drawable.
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+
+        // below line is use to create a bitmap for our
+        // drawable which we have added.
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+
+        // below line is use to add bitmap in our canvas.
+        Canvas canvas = new Canvas(bitmap);
+
+        // below line is use to draw our
+        // vector drawable in canvas.
+        vectorDrawable.draw(canvas);
+
+        // after generating our bitmap we are returning our bitmap.
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 
     @Override
@@ -89,6 +126,17 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
             case R.id.terrain_map:
                 mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                return true;
+            case R.id.custom_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                try {
+                    boolean success = mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.style_json));
+                    if (!success) {
+                        Log.e("MapsActivityRaw", "Style parsing failed.");
+                    }
+                } catch (Resources.NotFoundException e) {
+                    Log.e("MapsActivityRaw", "Can't find style.", e);
+                }
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
